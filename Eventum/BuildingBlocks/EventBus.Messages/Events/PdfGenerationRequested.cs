055f9a63bb4ab06953b58c03f2dad69809010aa1
Khaledov <khaled.m.ramadan@gmail.com>
2023-03-06 02:20:32 +0100

    adding events

---
+++ Eventum/BuildingBlocks/EventBus.Messages/Events/PdfGenerationRequested.cs
@@ -0,0 +1,13 @@
+﻿namespace EventBus.Messages.Events
+{
+    public class PdfGenerationRequested : IntegrationBaseEvent
+    {
+        public int CustomerNumber { get; set; }
+
+
+        public Guid DocumentNumber { get; set; }
+
+
+        public string? DocumentText { get; set; }
+    }
+}
---
+++ Eventum/Services/Subscription/Subscription.API/Models/SubscriptionRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Subscription.API.Models
+{
+    public class SubscriptionRequest
+    {
+        public int CustomerNumber { get; set; }
+        public Guid DocumentNumber { get; set; }
+        public string? DocumentText { get; set; }
+    }
+}
